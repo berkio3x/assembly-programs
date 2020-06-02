1b6e9319bc806ce3c71041eb6ec164d080abf9ea
kshitij <kshitij.singh@pyramidcyber.com>
2020-06-03 00:06:03 +0530

    add comments to dynamic string code

--- dynamic_string_length.asm
+++ dynamic_string_length.asm
@@ -2,27 +2,26 @@ section .text
 global _start
 
 _start:
-	mov ebx, msg
-	mov eax, ebx
+	mov ebx, msg  ; point ebx to the start of string 
+	mov eax, ebx  ; point eax to same location as eax
 nextchar:
-	cmp byte[eax], 0
-	jz finished
-	inc eax
-	jmp nextchar
+	cmp byte[eax], 0 ; compare the value pointed by eax with 0
+	jz finished     ; if same , we are done
+	inc eax		; else store the addess of next byte in eax
+	jmp nextchar	; loop until condition satisfies
 
 finished:
-
-	sub eax,ebx
-	mov edx, eax
+	sub eax,ebx	; find the striongi length
+	mov edx, eax	
 	mov ecx, msg
-	mov ebx , 1
-	mov eax, 4
-	int 80h
-	mov ebx, 0
-	mov eax, 1
-	int 80h
+	mov ebx , 1	; write to the stdout file
+	mov eax, 4	; invoke syswrite (kernel opcode 4)
+	int 80h		; raise a  interrupt
+	mov ebx, 0 	; load ebx with 0 to indicate proper return code
+	mov eax, 1	; load eax with 1, a call to sys_exit
+	int 80h		; raise an interrupt using int 80h
 	
 section .data
-	msg db "This is my world, I like pizzas!",0Ah
+	msg db "This is my world, I like pizzas!",0x0
 
 
